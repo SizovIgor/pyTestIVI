271ad6ac65823f0d6ed26e2a0731d31d4f267520
isizov <ya.shok.ya@yandex.ru>
2019-08-08 09:28:58 +0300

    Вынес настройки в отдельный файл

---
+++ source/option.py
@@ -0,0 +1,14 @@
+proxies = {
+    # "http": "http://isizov:aa11aa11@proxy.tsc.ts:8080",
+    # "https": "https://isizov:aa11aa11@proxy.tsc.ts:8080",
+}
+auth = ('ya.shok.ya@yandex.ru', 'hgJH768Cv23')
+
+base_url = 'http://rest.test.ivi.ru{}'
+characters = '/characters'
+character = '/character'
+characters_by_name = '/characters/{}'
+character_by_name = '/character/{}'
+reset = "/reset"
+session = None
+
