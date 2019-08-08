17dcbc1e63fbbb714a4275e1a38cf55fba080b52
isizov <ya.shok.ya@yandex.ru>
2019-08-08 09:28:26 +0300

    Вынес, по возможности, все фикстуры из тестов в отдельный файл

--- source/conftest.py
+++ source/conftest.py
@@ -1,14 +1,6 @@
 import pytest
 import requests
-
-# session = requests.session()
-# session = None
-proxies = {
-    # "http": "http://isizov:aa11aa11@proxy.tsc.ts:8080",
-    # "https": "https://isizov:aa11aa11@proxy.tsc.ts:8080",
-}
-
-auth = ('ya.shok.ya@yandex.ru', 'hgJH768Cv23')
+from source.option import auth, proxies
 
 
 @pytest.fixture(scope='module')
@@ -22,7 +14,7 @@ def create_session(request):
     return session
 
 
-@pytest.fixture(scope='module')
+@pytest.yield_fixture(scope='module')
 def delete_auth(create_session: requests.Session):
     create_session.auth = None
     print('Auth was deleted')
@@ -31,7 +23,7 @@ def delete_auth(create_session: requests.Session):
     print('Auth was restored')
 
 
-@pytest.fixture()
+@pytest.yield_fixture
 def post_setup(create_session: requests.Session) -> None:
     """
     Фикстура по обновлению заголовков для методов, на уровне теста, где необходимо отправлять json на сервер.
